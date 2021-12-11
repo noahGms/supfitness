c188cf5eede0757532d7df7e731b8cbda5a1172d
noahGms <noah@keshin.net>
2021-12-12 00:12:45 +0100

    created bottom nav bar with Weights, Curve and Races fragments

--- app/src/main/java/com/example/supfitness/MainActivity.kt
+++ app/src/main/java/com/example/supfitness/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.supfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainer)
+
+        var appBarConfiguration = AppBarConfiguration(setOf(R.id.weightsFragment, R.id.curveFragment, R.id.racesFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
