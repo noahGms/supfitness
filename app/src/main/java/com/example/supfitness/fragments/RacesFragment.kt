1d97ee93b43bd417b23ddac4fd0b6b085cf4343a
noahGms <noah@keshin.net>
2021-12-12 22:51:48 +0100

    added google map in races fragment

--- app/src/main/java/com/example/supfitness/fragments/RacesFragment.kt
+++ app/src/main/java/com/example/supfitness/fragments/RacesFragment.kt
@@ -6,12 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.supfitness.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-class RacesFragment : Fragment() {
+class RacesFragment : Fragment(), OnMapReadyCallback {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_races, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
     }
 }
